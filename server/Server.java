e9ee94b6443ac2668887afbfc182c46383b86d69
Baptiste Galmiche <baptiste.galmiche06@gmail.com>
2026-06-01 16:02:38 +0200

    rien

---
+++ server/Server.java
@@ -0,0 +1,23 @@
+package server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Server {
+static final int port = 6010 ;
+public static void main(String args[])
+throws SocketException, IOException {
+byte [] buffer = new byte [1024] ;
+String s ;
+DatagramSocket socket = new DatagramSocket(port) ;
+for ( ; ; ) {
+DatagramPacket packet =
+new DatagramPacket(buffer, buffer.length);
+socket.receive(packet) ;
+s = new String(packet.getData(), 0, packet.getLength(), "UTF-8");
+System.out.println(s) ;
+}
+}
+}
