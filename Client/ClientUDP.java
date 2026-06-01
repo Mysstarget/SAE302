4569b264e25e1f8d1e4830707b252bb66d8702ac
tom <iruling45@gmail.com>
2026-06-01 16:04:02 +0200

    commit

---
+++ Client/ClientUDP.java
@@ -0,0 +1,22 @@
+package com.mycompany.clientudp;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+public class ClientUDP {
+static final int port = 6010 ;
+public static void main(String args[])
+throws UnknownHostException, SocketException, IOException {
+InetAddress address = InetAddress.getByName("192.168.27.66") ;
+String msg="cest moi";
+int msglen = msg.length() ;
+byte [] message = new byte [msglen] ;
+msg.getBytes(0, msglen, message, 0) ;
+DatagramPacket packet =
+new DatagramPacket(message, msglen, address, port) ;
+DatagramSocket socket = new DatagramSocket() ;
+socket.send(packet) ;
+}
+}
